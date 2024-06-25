464c22e36b3ba44d644f4bab2310b314421e253c
ARUN KUMAR KUMMARI <113409292+kummariarunkumar@users.noreply.github.com>
2024-06-25 13:55:51 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,13 @@
+#Predict the output of the below code snippet. 
+
+for number in 10,15:
+    for counter in range(1,3):
+        print(number*counter, end=" ")
+
+'''
+options
+A. 10 20 30 15 30 45
+B. 10 15 20 30 30 45
+C. 10 15 20 30
+D. 10 20 15 30
+'''
