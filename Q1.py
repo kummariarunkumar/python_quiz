5e3beb73cd3f9704385e63d97ed392b81040a7a7
ARUN KUMAR KUMMARI <113409292+kummariarunkumar@users.noreply.github.com>
2024-06-25 13:52:13 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,25 @@
+#Predict the output of the below code snippet.
+
+a = -10
+b = -200
+c = 2000
+d = 4000
+if( a*b >=d):
+    if(d>c):
+        if(d%c!=0):
+            print(11)
+        else:
+            print(22)
+else:
+    if(b/a >0):
+        if(a<b or d%c!=0):
+          print(33)
+        else:
+          print(44)
+
+'''options
+A. 11
+B. 22
+C. 33
+D. 44 
+'''
